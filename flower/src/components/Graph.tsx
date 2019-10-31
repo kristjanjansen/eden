37248843f180393e58988527fee294f2ffe83dec
kristjanjansen <kristjan.jansen@gmail.com>
2019-10-31 21:39:14 +0100

    working on graph

--- flower/src/components/Graph.tsx
+++ flower/src/components/Graph.tsx
@@ -5,10 +5,15 @@ const GraphSvg = ({ layout }: any) => {
   const { width, height, children, edges } = layout;
   if (children)
     return (
-      <svg width={width} height={height} style={{ background: "gray" }}>
+      <svg
+        width={width}
+        height={height}
+        style={{ transform: "scale(2)", background: "gray" }}
+      >
         {children &&
-          children.map(({ x, y, width, height }: any) => (
+          children.map(({ x, y, width, height }: any, i: number) => (
             <rect
+              key={i}
               x={x}
               y={y}
               cx="5"
@@ -19,16 +24,42 @@ const GraphSvg = ({ layout }: any) => {
               fill="red"
             />
           ))}
+        {edges &&
+          edges.map(({ sections }: any, i: number) => (
+            <g key={i}>
+              {sections.map(({ startPoint, endPoint }: any, j: number) => (
+                <g key={i}>
+                  <circle
+                    cx={startPoint.x}
+                    cy={startPoint.y}
+                    r="3"
+                    fill="blue"
+                    opacity={1 / (i + 1)}
+                  />
+                  <circle
+                    cx={endPoint.x}
+                    cy={endPoint.y}
+                    r="3"
+                    fill="green"
+                    opacity={1 / (i + 1)}
+                  />
+                </g>
+              ))}
+            </g>
+          ))}
       </svg>
     );
   return <div></div>;
 };
 
-const calculateLines = (edges: any) => {
-  return edges.map((e: any) => {
-    return e;
-  });
-};
+// const calculateLines = (edges: any) => {
+//   return edges.map(({ sections }: any) => {
+//     if (sections.bendPoints) {
+
+//     }
+//     return [e.sections
+//   });
+// };
 
 const Graph: FC<{ layout: object }> = ({ layout }) => {
   return (
