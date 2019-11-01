adb7da13c0573bece8a876230f06fc2efa9834f3
kristjanjansen <kristjan.jansen@gmail.com>
2019-11-01 15:12:46 +0100

    fix logiline

--- flower/src/components/LogLine.tsx
+++ flower/src/components/LogLine.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from "react";
 
-const LogLine: FC = () => {
-  return <div>Hello</div>;
+const LogLine = ({ line }: any) => {
+  return (
+    <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
+      <div>{JSON.stringify(line)}</div>
+    </div>
+  );
 };
 
 export default LogLine;
