a42f8e80995d5131b130ffc88d76284893d59620
kristjanjansen <kristjan.jansen@gmail.com>
2019-11-02 09:40:25 +0100

    dark control

--- flower/src/containers/Layout.tsx
+++ flower/src/containers/Layout.tsx
@@ -4,6 +4,7 @@ import Menu from "../components/Menu";
 import Details from "../components/Details";
 
 import { colors } from "../styles/variables";
+import Slider from "../components/Slider";
 
 const Layout: FC<{
   children?: any;
@@ -18,13 +19,14 @@ const Layout: FC<{
   showDetails = false,
   onClose = () => null
 }) => {
-  const dark = false;
+  const [dark, setDark] = useState(0);
   return (
     <div
       style={{
         display: "flex",
         height: "100vh",
-        background: dark ? colors.darkestGray : colors.lightestGray
+        background: dark ? colors.darkestGray : colors.lightestGray,
+        position: "relative"
       }}
     >
       <div style={{ width: "200px", boxShadow: "5px 0 10px rgba(0,0,0,0.05)" }}>
@@ -51,6 +53,22 @@ const Layout: FC<{
           <Details onClose={() => onClose()}>{details}</Details>
         </div>
       )}
+
+      <div
+        style={{
+          position: "fixed",
+          left: "20px",
+          bottom: "20px",
+          width: "40px"
+        }}
+      >
+        <Slider
+          value={dark}
+          min={0}
+          max={1}
+          onChange={(value: number) => setDark(value)}
+        />
+      </div>
     </div>
   );
 };
