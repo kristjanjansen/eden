48e6540ae285377dfd9f7e14a3e65de02092e79a
kristjanjansen <kristjan.jansen@gmail.com>
2019-11-02 14:13:12 +0200

    moving out graphhtml renderer

--- flower/src/components/Graph.tsx
+++ flower/src/components/Graph.tsx
@@ -1,10 +1,8 @@
 import React, { FC, useEffect, useState } from "react";
 
-import GraphCard from "./GraphCard";
-import { useUiContext } from "../contexts/ui";
 import Slider from "./Slider";
+import GraphHtml from "./GraphHtml";
 import GraphSvg from "./GraphSvg";
-import { sectionLine } from "../utils/utils";
 
 // const sectionLine = ({ startPoint, endPoint, bendPoints = [] }: any) => {
 //   const makeLine = line().curve(curveCardinal.tension(0.92));
@@ -18,60 +16,60 @@ import { sectionLine } from "../utils/utils";
 //   return makeLine([start, end]);
 // };
 
-const GraphHtml = ({ layout, zoom = 1 }: any) => {
-  const { width, height, children, edges } = layout;
-  const [{ activeNodeIndex }, dispatch] = useUiContext();
+// const GraphHtml = ({ layout, zoom = 1 }: any) => {
+//   const { width, height, children, edges } = layout;
+//   const [{ activeNodeIndex }, dispatch] = useUiContext();
 
-  return (
-    <div
-      style={{
-        width: `${width}px`,
-        height: `${height}px`,
-        transform: `scale(${zoom})`
-      }}
-    >
-      {edges && (
-        <svg width={width} height={height} style={{ position: "absolute" }}>
-          {edges.map(({ sections }: any, i: number) => (
-            <g key={i}>
-              {sections.map((s: any, j: number) => (
-                <path
-                  key={j}
-                  d={sectionLine(s) || ""}
-                  opacity="0.1"
-                  strokeWidth="3"
-                  stroke="black"
-                  fill="none"
-                />
-              ))}
-            </g>
-          ))}
-        </svg>
-      )}
-      {children &&
-        children.map((node: any, i: number) => (
-          <div
-            key={i}
-            style={{
-              position: "absolute",
-              top: `${node.y}px`,
-              left: `${node.x}px`,
-              width: `${node.width}px`,
-              height: `${node.height}px`
-            }}
-          >
-            <GraphCard
-              node={node}
-              active={activeNodeIndex == i}
-              onClick={() =>
-                dispatch({ type: "activeNodeIndex", activeNodeIndex: i })
-              }
-            />
-          </div>
-        ))}
-    </div>
-  );
-};
+//   return (
+//     <div
+//       style={{
+//         width: `${width}px`,
+//         height: `${height}px`,
+//         transform: `scale(${zoom})`
+//       }}
+//     >
+//       {edges && (
+//         <svg width={width} height={height} style={{ position: "absolute" }}>
+//           {edges.map(({ sections }: any, i: number) => (
+//             <g key={i}>
+//               {sections.map((s: any, j: number) => (
+//                 <path
+//                   key={j}
+//                   d={sectionLine(s) || ""}
+//                   opacity="0.1"
+//                   strokeWidth="3"
+//                   stroke="black"
+//                   fill="none"
+//                 />
+//               ))}
+//             </g>
+//           ))}
+//         </svg>
+//       )}
+//       {children &&
+//         children.map((node: any, i: number) => (
+//           <div
+//             key={i}
+//             style={{
+//               position: "absolute",
+//               top: `${node.y}px`,
+//               left: `${node.x}px`,
+//               width: `${node.width}px`,
+//               height: `${node.height}px`
+//             }}
+//           >
+//             <GraphCard
+//               node={node}
+//               active={activeNodeIndex == i}
+//               onClick={() =>
+//                 dispatch({ type: "activeNodeIndex", activeNodeIndex: i })
+//               }
+//             />
+//           </div>
+//         ))}
+//     </div>
+//   );
+// };
 
 const Graph: FC<{ layout: any }> = ({ layout }) => {
   const [zoom, setZoom] = useState(1);
